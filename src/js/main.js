d9af3570f27c9e8aee075f5cee4f08d819084406
Sachitha Abesinghe <sachithasandakelumsl@gmail.com>
2024-01-12 12:16:14 +0530

    update search bar with abortable fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,54 @@
 const txtSearchElm = document.querySelector("#txt-search");
 const tblCustomersElm = document.querySelector("#tbl-customers");
+const loaderElm = document.querySelector("#loader");
 const API_BASE_URL = 'http://localhost:8080';
+
+let abortController = null;
 
 loadAllCustomers();
 
+
+
 function loadAllCustomers(query){
-    fetch(`${API_BASE_URL}/customers?page=1&size=50&q=${query ?? ''}`)
+    loaderElm.classList.remove('d-none')
+    if(abortController){
+        abortController.abort("Request aborted");
+    }
+
+    abortController = new AbortController();
+    const signal = abortController.signal;
+    fetch(`${API_BASE_URL}/customers?page=1&size=50&q=${query ?? ''}`,{signal})
         .then(req => req.json())
         .then(customerList => {
+            abortController=null;
             tblCustomersElm.querySelectorAll("tbody tr").forEach(tr=>tr.remove());
             customerList.forEach(c => addNewRow(c))
+            loaderElm.classList.add('d-none');
+            if(!customerList.length){
+                addNewRow(null);
+            }
             });
 }
 
 function addNewRow(customer){
     const trElm = document.createElement("tr");
     tblCustomersElm.querySelector('tbody').append(trElm);
-    trElm.innerHTML = `
+    if(customer){
+        trElm.innerHTML = `
         <td>${customer.id}</td>
         <td>${customer.firstName}</td>
         <td>${customer.lastName}</td>
         <td>${customer.contact}</td>
         <td>${customer.country}</td>
     `
+    } else {
+        trElm.innerHTML=`
+            <td colspan="5">
+                We're sorry. We cannot find any matches for your search term.
+            </td>
+        `
+    }
+
 }
 
 txtSearchElm.addEventListener('input',()=>{
